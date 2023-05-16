e524505a2bb7d6fc07cdd9cfc3fc88537963171e
nemanjamalesija <malesija13@gmail.com>
2023-05-16 18:23:17 +0200

    Add login page

---
+++ src/app/(dasboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { FC } from 'react';
+
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+
+  return <pre></pre>;
+};
+
+export default page;
