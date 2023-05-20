ee09e3d7d1d78c2686844c23a139d6e725c708f7
nemanjamalesija <malesija13@gmail.com>
2023-05-20 23:14:37 +0200

    Add comments

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+'use client';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
 type PageProps = {
   friends: User[];
 };
 
 const SidebarChatList = ({ friends }: PageProps) => {
+  const router = useRouter();
+
+  const [useenMessages, setUnseenMessages] = useState<Message[]>([]);
+
   return (
-    <ul role='list' className='max-h-[25] overflow-y-auto -mx-2 space-y-1'></ul>
+    <ul role='list' className='max-h-[25] overflow-y-auto -mx-2 space-y-1'>
+      {friends.sort().map((f) => {
+        return <div>ss</div>;
+      })}
+    </ul>
   );
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,21 @@ export default withAuth(
   async function middleware(req) {
     // get the path
     const pathname = req.nextUrl.pathname;
-    // manage route protection
+
+    // get token
     const isAuth = await getToken({ req });
+
+    //get the path name
     const isLoginPage = pathname.startsWith('/login');
+
     const sensitiveRoutes = ['/dashboard'];
+
+    // check if user is trying to acess sensitive route
     const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
       pathname.startsWith(route)
     );
+
+    // redirect to dashboard if logged in and pathname is /login
     if (isLoginPage) {
       {
         if (isAuth)
@@ -21,10 +29,12 @@ export default withAuth(
       return NextResponse.next();
     }
 
+    // redirect if not auth and trying to acess dashboard/:path*
     if (!isAuth && isAccessingSensitiveRoute)
       return NextResponse.redirect(new URL('/login', req.url));
 
-    if (pathname === '/')
+    // if logged in and acessing /
+    if (isAuth && pathname === '/')
       return NextResponse.redirect(new URL('/dashboard', req.url));
   },
   {
