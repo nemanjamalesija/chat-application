106d42df95619ab4997394e5565713e5aee50981
nemanjamalesija <malesija13@gmail.com>
2023-05-23 20:20:28 +0200

    Finish chat input component

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import Button from './ui/Button';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 type ChatInputProps = {
   chatPartner: User;
@@ -18,7 +19,13 @@ const ChatInput = ({ chatPartner, chatId }: ChatInputProps) => {
     setIsLoading(true);
     try {
       await axios.post('api/message/send', { text: input, chatId });
-    } catch (error) {}
+      setInput('');
+      textAreaRef.current?.focus();
+    } catch (error) {
+      toast.error('Something went wrong. Please try again later');
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
