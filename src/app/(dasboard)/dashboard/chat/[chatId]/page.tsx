c5dca2aa4df384e2ffb2d919df58d30a7ea5508b
nemanjamalesija <malesija13@gmail.com>
2023-05-23 14:05:30 +0200

    Display friend info

--- src/app/(dasboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dasboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from '@/lib/auth';
 import { messageArrayValidator } from '@/lib/validations/message';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
+import Image from 'next/image';
 
 type PageProps = {
   params: {
@@ -52,11 +53,40 @@ const Page = async ({ params }: PageProps) => {
   const chatPartner = (await fetchRedis(
     'get',
     `user:${chatParntnerId}`
-  )) as User;
+  )) as string;
+  const chatPartnerParsed = JSON.parse(chatPartner) as User;
 
   const initialMessages = await getChatMessages(chatId);
 
-  return <div>{params.chatId}</div>;
+  return (
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+        <div className='relative flex items-center space-x-4 '>
+          <div className='relative'>
+            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+              <Image
+                src={chatPartnerParsed.image}
+                alt={`${chatPartnerParsed.name} profile picture`}
+                fill
+                referrerPolicy='no-referrer'
+                className='rounded-full'
+              />
+            </div>
+          </div>
+          <div className='flex-flex-col leading-tight'>
+            <div className='text-xl flex items-center'>
+              <span className='text-gray-700 mr-3 font-semibold'>
+                {chatPartnerParsed.name}
+              </span>
+            </div>
+            <span className='text-sm text-gray-600'>
+              {chatPartnerParsed.email}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -31,8 +31,16 @@ const SidebarChatList = ({ friends, sessionId }: SidebarChatListProps) => {
 
         return (
           <li key={f.id} className=''>
-            <a href={`/dashboard/chat/${chatHrefConstructor(sessionId, f.id)}`}>
-              Hello
+            <a
+              className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 leading-6 font-semibold'
+              href={`/dashboard/chat/${chatHrefConstructor(sessionId, f.id)}`}
+            >
+              {f.name}
+              {unseenMessagesCount && (
+                <div className='bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center'>
+                  {unseenMessagesCount}
+                </div>
+              )}
             </a>
           </li>
         );
