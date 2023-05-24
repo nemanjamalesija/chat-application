68c4d8e3874c9090b438aed8738b4eb43bc84738
nemanjamalesija <malesija13@gmail.com>
2023-05-24 06:20:19 +0200

    Clean code

--- src/app/(dasboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dasboard)/dashboard/chat/[chatId]/page.tsx
@@ -61,19 +61,17 @@ const Page = async ({ params }: PageProps) => {
   const initialMessages = await getChatMessages(chatId);
 
   return (
-    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+    <div className='flex flex-1 flex-col justify-between h-full max-h-[calc(100vh-6rem)]'>
       <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
-        <div className='relative flex items-center space-x-4 '>
-          <div className='relative'>
-            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
-              <Image
-                fill
-                referrerPolicy='no-referrer'
-                src={chatPartnerParsed.image}
-                alt={`${chatPartnerParsed.name} profile picture`}
-                className='rounded-full'
-              />
-            </div>
+        <div className='flex items-center space-x-4 px-4'>
+          <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+            <Image
+              fill
+              referrerPolicy='no-referrer'
+              src={chatPartnerParsed.image}
+              alt={`${chatPartnerParsed.name} profile picture`}
+              className='rounded-full'
+            />
           </div>
           <div className='flex-flex-col leading-tight'>
             <div className='text-xl flex items-center'>
